09fe9af06e23e9b2717c950b05656ed3e9617d1d
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2021-12-16 09:01:37 +0000

    SnowFall

---
+++ SnowFall/code.js
@@ -0,0 +1,81 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":[],"propsByKey":{}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var grpSnow = createGroup();
+var snowFlake;
+createEdgeSprites();
+for (var i=0;i<50;i++) {
+  //fill("white");
+  snowFlake = createSprite(randomNumber(0,390), randomNumber(0,390),10,10);
+  snowFlake.shapeColor="white";
+  snowFlake.velocityY = 1;
+  grpSnow.add(snowFlake);
+}
+
+
+function draw() {
+  //logic
+  for (var i = 0; i < 50; i++ ){
+    if (grpSnow.get(i).isTouching(bottomEdge)) {
+      grpSnow.get(i).x = randomNumber(0,390);
+      grpSnow.get(i).y = 0;
+    }
+  }
+  
+  background("black");
+  drawSprites();
+
+  
+}
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
